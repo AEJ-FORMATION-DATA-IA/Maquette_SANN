042ec8dde514ad846170265ead4414815fff75d2
Sann Shabib Yussuf <72591220+sangreck@users.noreply.github.com>
2021-08-29 17:20:46 +0000

    Maquette_SANN

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+
+    document.documentElement.style.setProperty('--besta-color', 'green');
+    document.documentElement.style.setProperty('--main-color', 'white');
+    document.documentElement.style.setProperty('--brd-color', 'gray');
+
+})
